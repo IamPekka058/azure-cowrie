405865908f88a2505340e518488ad1ff21f7eede
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2026-03-19 17:17:41 +0100

    ✨ Add Cowrie analysis workbook and integrate with Application Insights

---
+++ workbook.tf
@@ -0,0 +1,15 @@
+resource "azurerm_application_insights_workbook" "cowrie_dashboard" {
+  name                = uuid()
+  resource_group_name = azurerm_resource_group.cowrie.name
+  location            = var.location
+  display_name        = "Cowrie Honeypot Analysis"
+
+  source_id = lower(azurerm_log_analytics_workspace.cowrie.id)
+  category  = "workbook"
+
+  data_json = file("${path.module}/assets/Analysis.workbook")
+
+  tags = {
+    Project = "Honeypot"
+  }
+}
